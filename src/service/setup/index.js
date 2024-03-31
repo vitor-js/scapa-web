f12086c7459254be854138a76163095e89dfadd0
vitor melo <vitor.melo@sempreceub.com.br>
2024-03-31 15:15:41 -0300

    fix: change name baseURl

--- src/service/setup/index.js
+++ src/service/setup/index.js
@@ -5,7 +5,7 @@ import { getCookies } from 'cookies-next';
 
 
 export function setupApiClient() {
-    const baseURL = "http://localhost:4040"
+    const baseURL = "https://scapa-api-0b5e0c4e61f2.herokuapp.com"
     const api = axios.create({
         baseURL,
     });
