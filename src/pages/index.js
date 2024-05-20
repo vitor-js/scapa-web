4b8d1a3250454f2266f3f82ec5f9d4881e2e6deb
vitor melo <vitor.melo@sempreceub.com.br>
2024-05-20 00:16:43 -0300

    feat: finish first part

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 
-import Login from '../pages/login'
+import { Flex } from '@chakra-ui/react';
 
-export default function Index() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+export default function Home() {
+  return <Flex />;
 }
+
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: true,
+    },
+  };
+};
